3a77005bea72c630385312cdb31a2444a8595a95
Colten Krauter <18080897+coltenkrauter@users.noreply.github.com>
2024-09-27 09:27:49 -0600

    feat: update linter

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,20 +1,20 @@
-import { expect } from 'chai';
-import { spy } from 'sinon';
+import { expect } from 'chai'
+import { spy } from 'sinon'
 
 function add(a: number, b: number): number {
-    return a + b;
+	return a + b
 }
 
 describe('add function', () => {
-    it('should return the sum of two numbers', () => {
-        const result = add(2, 3)
-        expect(result).to.equal(5)
-    })
+	it('should return the sum of two numbers', () => {
+		const result = add(2, 3)
+		expect(result).to.equal(5)
+	})
 
-    it('should be called once', () => {
-        const spyAdd = spy(add)
-        const result = spyAdd(2, 3)
-        expect(result).to.equal(5)
-        expect(spyAdd.calledOnce).to.be.true
-    })
+	it('should be called once', () => {
+		const spyAdd = spy(add)
+		const result = spyAdd(2, 3)
+		expect(result).to.equal(5)
+		expect(spyAdd.calledOnce).to.be.true
+	})
 })

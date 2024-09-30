589cf432bb6ca255b09f6f2ad74e3233add42bd9
Colten Krauter <18080897+coltenkrauter@users.noreply.github.com>
2024-09-29 22:55:13 -0600

    refactor: switch to CommonJs and Jest

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,20 +1,19 @@
-import { expect } from 'chai'
-import { spy } from 'sinon'
+import { describe, it, expect, jest } from '@jest/globals'
 
-function add(a: number, b: number): number {
-	return a + b
+function add(a: number, b: number) {
+  return a + b
 }
 
 describe('add function', () => {
-	it('should return the sum of two numbers', () => {
-		const result = add(2, 3)
-		expect(result).to.equal(5)
-	})
+  it('should return the sum of two numbers', () => {
+    const result = add(2, 3)
+    expect(result).toBe(5)
+  })
 
-	it('should be called once', () => {
-		const spyAdd = spy(add)
-		const result = spyAdd(2, 3)
-		expect(result).to.equal(5)
-		expect(spyAdd.calledOnce).to.be.true
-	})
+  it('should be called once', () => {
+    const spyAdd = jest.fn(add)
+    const result = spyAdd(2, 3)
+    expect(result).toBe(5)
+    expect(spyAdd).toHaveBeenCalledTimes(1)
+  })
 })
